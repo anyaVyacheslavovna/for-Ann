7488b0bd387074266f7de5160aa579cd983dc0f9
6thSence <d.pushkarskaya.pr@gmail.com>
2016-08-17 23:32:50 +0700

    it's just pipe

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var concatCss = require('gulp-concat-css');
+var rename = require('gulp-rename');
+
+// task, src, dest, pipe, watch, run
+
+gulp.task('default', ['build', 'watch']);
+
+gulp.task('build', function() {
+    gulp.src('src/*.css')
+        .pipe(concatCss('style.css'))
+        .pipe(rename('build.css'))
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/style.css', function() {
+        gulp.run('build');
+    })
+});
